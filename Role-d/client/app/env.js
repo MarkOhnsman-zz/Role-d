2b73439d1b62c1b27e08f64a341c79d6b67c3de2
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-07-12 09:46:40 -0600

    builds done

--- Role-d/client/app/env.js
+++
@@ -1,6 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'dev-jifdsfgm.us.auth0.com'
-export const clientId = 'kdA2drx86aMYWundbilyJsAPj43nWovV'
-export const audience = 'https://MarkDev.com'
-
---
+++ Role-d/client/assets/AboutPage.e03331a7.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.85f3f338.js";import{o,b as t,d as s}from"./vendor.19873f3d.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,f,p,u,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
